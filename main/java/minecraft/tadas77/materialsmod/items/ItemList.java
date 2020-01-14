879a0df1364e188d97e8f8cf82238f95e6004794
Tadas <t.vilemaitis@gmail.com>
2020-01-14 16:15:11 -0500

    do something

--- main/java/minecraft/tadas77/materialsmod/items/ItemList.java
+++ main/java/minecraft/tadas77/materialsmod/items/ItemList.java
@@ -10,6 +10,7 @@ public class ItemList extends MinecraftList {
 
 	public static void register(final RegistryEvent.Register<Item> event) {
 		ItemList.copper = new Item((new Item.Properties()).group(ItemGroup.MATERIALS)).setRegistryName(location("copper_ingot"));
+		
 		event.getRegistry().registerAll(
 				ItemList.copper
 		);
