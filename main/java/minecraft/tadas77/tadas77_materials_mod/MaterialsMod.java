0eccfcec3e6fb9457f5e16bd61b922ef6c215e5f
Jay jayjay <jayjay.jay20162016@gmail.com>
2020-01-30 17:14:23 -0500

    Implement new system with items and blocks.

--- main/java/minecraft/tadas77/tadas77_materials_mod/MaterialsMod.java
+++ main/java/minecraft/tadas77/tadas77_materials_mod/MaterialsMod.java
@@ -3,14 +3,7 @@ package minecraft.tadas77.tadas77_materials_mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import minecraft.tadas77.tadas77_materials_mod.blocks.BlockList;
-import minecraft.tadas77.tadas77_materials_mod.items.ItemList;
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -41,19 +34,7 @@ public class MaterialsMod {
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void registerItems(final RegistryEvent.Register<Item> event) {
-			ItemList.register(event);
-		}
 
-		@SubscribeEvent
-		public static void registerBlocks(final RegistryEvent.Register<Block> event) {
-			BlockList.register(event);
-		}
-		
-		public static ResourceLocation location(String loc) {
-			return new ResourceLocation(MaterialsMod.MODID, loc);
-		}
 	}
 
 }
