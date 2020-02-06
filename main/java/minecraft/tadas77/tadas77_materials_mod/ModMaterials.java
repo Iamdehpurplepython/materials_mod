c981852cfeeb96f74e9f78a86ebd173d3c1a3ec7
jay20162016 <jayjay.jay20162016@gmail.com>
2020-02-06 08:02:37 -0500

    Update ModMaterials.java

--- main/java/minecraft/tadas77/tadas77_materials_mod/ModMaterials.java
+++ main/java/minecraft/tadas77/tadas77_materials_mod/ModMaterials.java
@@ -9,7 +9,7 @@ import net.minecraftforge.common.ToolType;
 /*
  * 
  * Copper: (204, 102, 0, 255)
- * Plastic: (255, 106, 77, 225)
+ * (Partially) Transparent Plastic: (255, 106, 77, 225)
  * 
  */
 
@@ -19,18 +19,26 @@ public class ModMaterials {
 	public static void init() {
 		MaterialsMod.LOGGER.debug("ModMaterials Init - 3.14159265");
 
-		ModMaterials.copper = ModMaterials.createRegularMaterial("copper", 3.0F,
-				30.0F, 1, ToolType.PICKAXE);
+		ModMaterials.copper = ModMaterials.createRegularMaterial("copper", Material.IRON, 1, ToolType.PICKAXE);
 	}
 
-	public static JayMaterial createRegularMaterial(String locname,
+	public static JayMaterial createRegularMaterial(String locname, Material mat
 			Float hardness, Float resistance, int harvestlevel,
 			ToolType harvesttool) {
 		return new JayMaterial(locname,
 				new Item.Properties().group(ItemGroup.MATERIALS),
-				Block.Properties.create(Material.IRON)
+				Block.Properties.create(mat)
 						.hardnessAndResistance(hardness, resistance)
 						.harvestLevel(harvestlevel).harvestTool(harvesttool),
 				new Item.Properties().group(ItemGroup.BUILDING_BLOCKS));
+	}
+	
+	public static JayMaterial createRegularMaterial(String locname, Material mat, int harvestlevel, ToolType harvesttool) {
+		return new JayMaterial(locname,
+			new Item.Properties().group(ItemGroup.MATERIALS),
+			Block.Properties.create(mat)
+					.hardnessAndResistance(30.0F, 3.0F)
+					.harvestLevel(harvestlevel).harvestTool(harvesttool),
+			new Item.Properties().group(ItemGroup.BUILDING_BLOCKS));
 	}
 }
