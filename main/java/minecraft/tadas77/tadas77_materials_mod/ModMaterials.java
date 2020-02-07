c0561521dca4ad1de011f19fa7e88bc532d1278b
Jay jayjay <jayjay.jay20162016@gmail.com>
2020-02-07 17:19:11 -0500

    Fix copper taking forever to break; error: hardness + resistance values

--- main/java/minecraft/tadas77/tadas77_materials_mod/ModMaterials.java
+++ main/java/minecraft/tadas77/tadas77_materials_mod/ModMaterials.java
@@ -2,6 +2,7 @@ package minecraft.tadas77.tadas77_materials_mod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.common.ToolType;
 
@@ -20,7 +21,7 @@ public class ModMaterials {
 
 		ModMaterials.copper = new JayMaterial("copper",
 				Block.Properties.create(Material.IRON)
-						.hardnessAndResistance(30.0F, 3.0F).harvestLevel(1)
+						.hardnessAndResistance(5.0F, 6.0F).harvestLevel(1)
 						.harvestTool(ToolType.PICKAXE),
 				new JayItemTier(2, 200, 5.5F, 1.5F, 12, () -> {
 					return Ingredient.fromItems(ModMaterials.copper.item);
